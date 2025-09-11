b2789dc1f027aa50d0922a4c29c98d4f61023f03
Jordan Sivils <jordan.sivils.wd@gmail.com>
2025-09-10 21:30:29 -0400

    fixing image path

--- client/src/components/ExceltoPdf/UploadDirections.tsx
+++ client/src/components/ExceltoPdf/UploadDirections.tsx
@@ -15,23 +15,23 @@ const UploadDirections = () => {
             <div className={`flex-col`}>
                 <p>Step 2:</p>
                 <p>Select the gear Icon in the top right corner.</p>
-                <img src="src/assets/Setting_Icon.webp" alt="Gear Icon" height={50} width={70} />
+                <img src="/assets/Setting_Icon.webp" alt="Gear Icon" height={50} width={70} />
             </div>
         </li>
         <li>
             <div className={`flex-col`}>
                 <p>Step 3:</p>
                 <p>a: Select the Records Per Page and make sure the number is greater than the number of records you want to print.</p>
-                <img src="src/assets/Table_Settings.webp" alt="Gear Icon" height={300} width={500} />
+                <img src="/assets/Table_Settings.webp" alt="Gear Icon" height={300} width={500} />
                 <p>b: Select the Show columns and make sure that suppliers is selected so you can sort by the targeted supplier.</p>
-                <img src="src/assets/Select_Supplier.webp" alt="Gear Icon" height={180} width={500} />
+                <img src="/assets/Select_Supplier.webp" alt="Gear Icon" height={180} width={500} />
             </div>
         </li>
         <li>
             <div className={`flex-col`}>
                 <p>Step 4:</p>
                 <p>Highlight the rows you want to print for getting Invetory Counts. You can skip lines you dont want.</p>
-                <img src="src/assets/Highlighted_lines_Redacted.webp" alt="Gear Icon" height={300} width={500} />
+                <img src="/assets/Highlighted_lines_Redacted.webp" alt="Gear Icon" height={300} width={500} />
                 <p>They are marked out because of where I host the codebase, and help I may get</p>
             </div>
         </li>
@@ -39,9 +39,9 @@ const UploadDirections = () => {
             <div className={`flex-col`}>
                 <p>Step 5:</p>
                 <p>A: Select the Smart Export Icon.</p>
-                <img src="src/assets/Smart_Export.webp" alt="Gear Icon" height={50} width={70} />
+                <img src="/assets/Smart_Export.webp" alt="Gear Icon" height={50} width={70} />
                 <p>B: Select the Options for "Selected Records"! This will immediately create an excel file with the selected lines.</p>
-                <img src="src/assets/Correct_Export.webp" alt="Gear Icon" height={300} width={300} />
+                <img src="/assets/Correct_Export.webp" alt="Gear Icon" height={300} width={300} />
             </div>
         </li>
         <li>
