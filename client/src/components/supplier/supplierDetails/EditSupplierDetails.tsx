6aa101231a101c6c24c9733261af1c3e696f2e1a
Jordan Sivils <jordan.sivils.wd@gmail.com>
2025-11-14 20:11:06 -0500

    supplier details

--- client/src/components/supplier/supplierDetails/EditSupplierDetails.tsx
+++ client/src/components/supplier/supplierDetails/EditSupplierDetails.tsx
@@ -1,6 +1,21 @@
-const EditSupplierDetails = () => {
+import type { Supplier } from "../types/listResponse"
+
+type EditSupplierDetailProps = {
+    data: Supplier
+}
+
+const EditSupplierDetails = ({ data }: EditSupplierDetailProps) => {
     return(
-        <h1>Edit Supplier details...</h1>
+        <div>
+            <h4>
+                under constuction
+            </h4>
+            <ul>
+                <h4>{data.name}</h4>
+                <li>{data.supplierDetails?.orderMinimum}</li>
+            </ul>
+        </div>
+        
     )
 }
 
